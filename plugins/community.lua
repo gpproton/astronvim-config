b0ff2556a310038c9ba3f92c5301105c5663277a
Grayscale Pacifista <gpproton@live.com>
2024-03-04 00:56:43 +0100

    Added required language community packs

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,9 +1,24 @@
 return {
-  -- Add the community repository of plugin specifications
-  "AstroNvim/astrocommunity",
-  -- example of importing a plugin, comment out to use it or add your own
-  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.pack.ansible" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.cmake" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.cs" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.vue" },
+  { import = "astrocommunity.pack.angular" },
+  { import = "astrocommunity.pack.ps1" },
+  { import = "astrocommunity.pack.toml" },
+  -- { import = "astrocommunity.pack.php" },
+  -- { import = "astrocommunity.pack.proto" },
+  -- { import = "astrocommunity.pack.java" },
+  -- { import = "astrocommunity.pack.kotlin" },
 }
